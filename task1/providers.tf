23f421a649ee2bbedc7557778196a88af0629a80
Roman Kostiv <kostiv.rr2005@gmail.com>
2026-04-04 16:09:27 +0300

    Fix terraform s3 backend endpoint syntax

--- task1/providers.tf
+++ task1/providers.tf
@@ -8,18 +8,17 @@ terraform {
 
   # Зберігання tfstate у хмарі (DigitalOcean Spaces сумісний з S3)
   backend "s3" {
-    # ОНОВЛЕНО: Тепер використовуємо блок endpoints та обов'язково додаємо https://
     endpoints = {
-      s3 = "https://kostiv-bucket.fra1.digitaloceanspaces.com"
+      s3 = "https://fra1.digitaloceanspaces.com"
     }
     region                      = "us-east-1" 
-    bucket                      = "kostiv-bucket"
+    bucket                      = "kostiv-bucket" 
     key                         = "exam/terraform.tfstate"
     skip_credentials_validation = true
     skip_requesting_account_id  = true
     skip_metadata_api_check     = true
     skip_s3_checksum            = true
-    skip_region_validation      = true # Додано для більшої стабільності з DO
+    skip_region_validation      = true
   }
 }
 
